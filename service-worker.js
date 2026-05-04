7d6390264aaab70bab703d1734cdf9dcfefb50bf
Tim <Tim.Red@web.de>
2026-05-04 14:16:22 +0200

    release button

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,8 @@
-const CACHE_NAME = 'volumetric-time-camera-v1.3';
+const CACHE_NAME = 'volumetric-time-camera-v1.5';
 const APP_SHELL = [
   './',
   './index.html',
+  './styles.css',
   './manifest.json',
   './icons/icon_32.png',
   './icons/icon_192.png',
@@ -12,6 +13,9 @@ const APP_SHELL = [
   './icons/gallery.png',
   './icons/aperture.png'
 ];
+const APP_SHELL_URLS = new Set(
+  APP_SHELL.map((asset) => new URL(asset, self.location.href).href)
+);
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
@@ -45,24 +49,40 @@ self.addEventListener('fetch', (event) => {
     return;
   }
 
-  event.respondWith(
-    caches.match(request).then((cachedResponse) => {
-      if (cachedResponse) {
-        return cachedResponse;
-      }
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((networkResponse) => {
+          if (networkResponse && networkResponse.status === 200) {
+            const responseClone = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put('./index.html', responseClone).catch(() => {});
+            });
+          }
 
-      return fetch(request).then((networkResponse) => {
-        if (!networkResponse || networkResponse.status !== 200) {
           return networkResponse;
+        })
+        .catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  if (!APP_SHELL_URLS.has(requestUrl.href)) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cachedResponse) => (
+      cachedResponse || fetch(request).then((networkResponse) => {
+        if (networkResponse && networkResponse.status === 200) {
+          const responseClone = networkResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(request, responseClone).catch(() => {});
+          });
         }
 
-        const responseClone = networkResponse.clone();
-        caches.open(CACHE_NAME).then((cache) => {
-          cache.put(request, responseClone).catch(() => {});
-        });
-
         return networkResponse;
-      }).catch(() => caches.match('./index.html'));
-    })
+      })
+    ))
   );
 });
